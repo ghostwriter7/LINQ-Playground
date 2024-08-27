b0302def6508976e9ca864ecc79b0bd3e6039761
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 15:45:48 +0200

    Set methods explained

---
+++ Playground/Operators/Set08.cs
@@ -0,0 +1,15 @@
+﻿namespace Playground.Operators;
+
+public class Set08
+{
+    public void Demo()
+    {
+        var word1 = "hellooooo";
+        var word2 = "help";
+
+        var distinctChars = word1.Distinct();
+        var commonChars = word1.Intersect(word2);
+        var allUniqueChars = word1.Union(word2);
+        var uniqueCharsFromWord1 = word1.Except(word2);
+    }
+}
