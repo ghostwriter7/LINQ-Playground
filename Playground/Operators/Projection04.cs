c9bc7a99cf4ee5b393bf386a7db9ee91d5fb62ea
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 08:59:46 +0200

    Projection demonstrated (Select / SelectMany)

---
+++ Playground/Operators/Projection04.cs
@@ -0,0 +1,25 @@
+﻿namespace Playground.Operators;
+
+public class Projection04
+{
+    public static void SelectUsage()
+    {
+        var ints = Enumerable.Range(1, 3);
+        var squares = ints.Select(x => x * x);
+
+        var text = "This is a sample sentence";
+        var wordLengths = text.Split().Select(word => word.Length);
+
+        var wordsAndLengths = text.Split().Select(word => new { Word = word, Size = word.Length });
+
+        var rand = new Random();
+        var randomNumbers = Enumerable.Range(1, 10).Select(_ => rand.Next(100));
+
+        var strings = new[] { "orange,apple,strawberry", "blueberry", "banana,nuts" };
+        var flatList = strings.SelectMany(str => str.Split(','));
+
+        string[] colors = { "blue", "orange", "red" };
+        string[] objects = { "car", "house", "plane" };
+        var carthesianProduct = colors.SelectMany(_ => objects, (c, o) => new { Color = c, Obj = o });
+    }
+}
