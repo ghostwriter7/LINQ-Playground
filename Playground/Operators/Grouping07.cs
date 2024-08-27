e8c4c5dac8558c78db2f77f24ca46965fa7a05a9
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 14:57:46 +0200

    Grouping demonstrated

---
+++ Playground/Operators/Grouping07.cs
@@ -0,0 +1,29 @@
+﻿namespace Playground.Operators;
+
+public class Grouping07
+{
+    public void Demo()
+    {
+        var people = new List<Person>()
+        {
+            new("Jack", 25),
+            new("Stephan", 25),
+            new("Tom", 35),
+            new("John", 18),
+            new("Hulk", 42)
+        };
+
+        var byName = people.GroupBy(p => p.Name);
+        var olderBy30Groups = people.GroupBy(p => p.Age > 30);
+        var namesByAge = people.GroupBy(p => p.Age, p => p.Name);
+
+        foreach (var item in byName)
+        {
+            Console.WriteLine($"These people share the name {item.Key}");
+            foreach (var person in item)
+                Console.WriteLine($"{person.Name} is {person.Age}");
+        }
+    }
+    
+    private record Person(string Name, int Age);
+}
