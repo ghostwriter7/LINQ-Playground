c0cd15ec451ed5ee9fa1e7b8360f7ffda3582544
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 15:52:57 +0200

    Qualifying methods explained

---
+++ Playground/Operators/Quantifying09.cs
@@ -0,0 +1,13 @@
+﻿namespace Playground.Operators;
+
+public class Quantifying09
+{
+    public void Demo()
+    {
+        int[] ints = { 1, 2, 3, 4, 5, 6 };
+        var allIntsGreaterThanZero = ints.All(x => x > 0);
+        var anyNumberLessThanTwo = ints.Any(x => x < 2);
+        var isCollectionEmpty = ints.Any();
+        var doesContainThree = ints.Contains(3);
+    }
+}
