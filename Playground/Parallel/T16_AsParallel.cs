3ac55f1e1a17f681cf68af48be33bb415f0b056b
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 20:14:15 +0200

    AsParallel demonstrated

---
+++ Playground/Parallel/T16_AsParallel.cs
@@ -0,0 +1,21 @@
+﻿namespace Playground.Parallel;
+
+public class T16_AsParallel
+{
+    public static void Demo()
+    {
+        const int count = 50;
+        var ints = Enumerable.Range(1, count);
+        var results = new int[count];
+        
+        ints.AsParallel().ForAll(item =>
+        {
+            var result = item * item * item;
+            Console.WriteLine($"Result: {result} computed by ({Task.CurrentId})");
+            results[item - 1] = result;
+        });
+
+        // to enforce picking up items in order by threads
+        var cubes = ints.AsParallel().AsOrdered().Select(x => x * x * x);
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Playground.Parallel;
+
+T16_AsParallel.Demo();
