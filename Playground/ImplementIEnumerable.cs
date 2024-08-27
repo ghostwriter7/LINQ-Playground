df4ee9708fd9a84bf1d77df9588da067dd1ff055
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 08:20:37 +0200

    Implemented IEnumerable<T> manually

---
+++ Playground/ImplementIEnumerable.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace Playground;
+
+public class ImplementIEnumerable
+{
+    public static void Demo()
+    {
+        var parameters = new Params("a", "b", "c");
+        foreach (var param in parameters)
+        {
+            Console.WriteLine($"Parameter is {param}");
+        }
+
+        var person = new Person("Jack", "Robins", "Pinkman");
+        var hasJackName = person.Names.Contains("Jack");
+    }
+
+    private class Person
+    {
+        private readonly string _firstName, _middleName, _lastName;
+
+        public Person(string firstName, string middleName, string lastName)
+        {
+            _firstName = firstName;
+            _middleName = middleName;
+            _lastName = lastName;
+        }
+
+        public IEnumerable<string> Names
+        {
+            get
+            {
+                yield return _firstName;
+                yield return _middleName;
+                yield return _lastName;
+            }
+        }
+    }
+    
+    public class Params : IEnumerable<string>
+    {
+        private readonly string _param1, _param2, _param3;
+
+        public Params(string param1, string param2, string param3)
+        {
+            _param1 = param1;
+            _param2 = param2;
+            _param3 = param3;
+        }
+
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            yield return _param1;
+            yield return _param2;
+            yield return _param3;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+﻿# Basic
+
+LINQ - Language INtegreated Query
+
+How is LINQ implemented?
+
+1. Extension methods (called operators)
+2. Query expression syntax (SQL-like)
+
+### Operators
+
+Higher-order functions (functions taking other functions)
+
+### IEnumerable<T> - LINQ to Objects
+
+1. Implemented by all .NET collection types (arrays, List<T> etc.)
+2. LINQ operators are impl as extensions on it
+
+### `IQueryable<T>` - LINQ to Entities
+
+1. It will get turned into SQL queries
+
+### `Enumerable` 
+
+1. Class, not an interface
+2. Contains static generators for sequences
+3. Contains LINQ extension method definitions
