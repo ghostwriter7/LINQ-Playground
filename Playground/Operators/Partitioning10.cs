f9c5b50298ab2b96410d0cba67eeef46aa5fe1cc
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 15:55:48 +0200

    Partitioning methods explained

---
+++ Playground/Operators/Partitioning10.cs
@@ -0,0 +1,18 @@
+﻿namespace Playground.Operators;
+
+public class Partitioning10
+{
+    public static void Demo()
+    {
+        var ints = Enumerable.Range(1, 100);
+
+        var skip10Items = ints.Skip(10);
+        var takeNext20Items = skip10Items.Take(20);
+
+        var skipNumbersLessThan20 = ints.SkipWhile(x => x <= 20);
+        var takeNumbersLessThan50 = ints.TakeWhile(x => x <= 50);
+
+        var skip20LastItems = ints.SkipLast(20);
+        var take20LastItems = ints.TakeLast(20);
+    }
+}
