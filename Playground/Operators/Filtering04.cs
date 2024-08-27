6819a43b89353468a32771312bc3412129f2eb73
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-08-27 12:29:28 +0200

    Filtering demonstrated

---
+++ Playground/Operators/Filtering04.cs
@@ -0,0 +1,15 @@
+﻿namespace Playground.Operators;
+
+public class Filtering04
+{
+    public static void Demo()
+    {
+        var ints = Enumerable.Range(1, 10);
+        var evens = ints.Where(x => x % 2 == 0);
+        var oddSquares = ints.Select(x => x * x).Where(x => x % 2 == 1);
+
+        object[] objects = { 1, 1.25, 3, 3.55f, "hello world" };
+        var onlyInts = objects.OfType<int>();
+        var onlyFloats = objects.OfType<float>();
+    }
+}
